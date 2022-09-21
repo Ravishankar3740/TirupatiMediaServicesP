b3720b16b1ceabc17492f32f7bfe2f5781e99318
ravishankarbirajdar <ravi.birajdar8880@gmail.com>
2022-09-21 21:07:19 +0530

    TMS-15: 1

--- paging/views.py
+++ paging/views.py
@@ -131,11 +131,11 @@ class SendleadInPdf(APIView):
             pp.close()
             faacebook_page = FacebookPages.objects.get(facebook_page=ad_name)
             url = "https://betablaster.in/api/send.php"
-            params = {"number": "91" + "9069505151", "type": "media", "filename":ad_name+".pdf","message":"Send by python for testing","media_url": "http://www.leadapp.gruhkhoj.in/media/"+ad_name+".pdf",
+            params = {"number": "91" + faacebook_page.whats_app_number, "type": "media", "filename":ad_name+".pdf","message":"Send by python for testing","media_url": "http://www.leadapp.gruhkhoj.in/media/"+ad_name+".pdf",
                       "instance_id": "6329BF36277A7", "access_token": "88b2435f1513c443ca80703de1b67943"}
             data = requests.post(url, params=params, verify=False)
             if request.POST.get('getown'):
-                params = {"number": "91" +"9069505151", "type": "media", "filename": ad_name+".pdf",
+                params = {"number": "91" +"7020740986", "type": "media", "filename": ad_name+".pdf",
                           "message": "Send by python for testing", "media_url": "http://www.leadapp.gruhkhoj.in/media/"+ad_name+".pdf",
                           "instance_id": "6329BF36277A7", "access_token": "88b2435f1513c443ca80703de1b67943"}
                 data = requests.post(url, params=params, verify=False)
